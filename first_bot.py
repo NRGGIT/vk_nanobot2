55783f11a9da57751eb99364e1110f91ccc4d839
NRGCODER <44064736+NRGCODER@users.noreply.github.com>
2019-07-02 10:56:13 +0500

    second commit

--- first_bot.py
+++ first_bot.py
@@ -5,7 +5,7 @@ from config import *
 vkBot = vk_api.VkApi(token = BOT_TOKEN)
 
 USER_ID = 2299551 #здесь пишите свой id
-text = 'Это сообщение отправленно с помощью vk_api'
+text = 'Это сообщение отправленно с помощью vk_api 222222'
 result = vkBot.method('messages.send', {'user_id' : USER_ID,
                                         'random_id' : random.randint(1, 2147483648),
                                         'message' : text})
